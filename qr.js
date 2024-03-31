27f39aa1fb9cf05e87145e49853c5ddf053286c4
Rhodvick <150789919+Rhodvick@users.noreply.github.com>
2024-03-31 08:32:35 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -68,9 +68,9 @@ ______________________________________
 ╰═════════════⊷
 ╔═════◇
 ║  『••• OWNER INFO•••』
-║❒ *Instagram:* _https://www.instagram.com/ibrahim_tech_official?igsh=MWt3bXY1NnVkNWd6Mg==_
+║❒ *Instagram:* _https://www.instagram.com/rhodvickjunior?igsh=YzljYTk1ODg3Zg==_
 ║❒ *Owner:* _https://wa.me/254115790827_
-║❒ *Repo:* _https://github.com/Rhodvick/_
+║❒ *Repo:* _https://github.com/Rhodvick/RHODVICK-AI_
 ║❒ *WaGroup:* _https://chat.whatsapp.com/Dc7OCtGmAI2BNtkKbGSJy_
 ║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D_
 ║❒ *YouTube:* _https://www.youtube.com/@Rhodvick
