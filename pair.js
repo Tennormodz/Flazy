233059b3e545032b2727d6463c81b5be1809fd02
gidiowis <160538903+gidiowis@users.noreply.github.com>
2024-07-05 00:29:18 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: France_King,
+    default: Giddy_Tennor,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
@@ -20,13 +20,13 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function FLASH_MD_PAIR_CODE() {
+        async function Ultraz_MD_PAIR_CODE() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let Pair_Code_By_France_King = France_King({
+            let Pair_Code_By_Giddy_Tennor = Giddy_Tennor({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
@@ -35,10 +35,10 @@ router.get('/', async (req, res) => {
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: ["Chrome (Linux)", "", ""]
              });
-             if(!Pair_Code_By_France_King.authState.creds.registered) {
+             if(!Pair_Code_By_Giddy_Tennor.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_France_King.requestPairingCode(num)
+                            const code = await Pair_Code_By_Giddy_Tennor.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
@@ -54,38 +54,38 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, { text: '' + b64data });
+               let session = await Pair_Code_By_Giddy_Tennor.sendMessage(Pair_Code_By_Giddy_Tennor.user.id, { text: '' + b64data });
 
-               let FLASH_MD_TEXT = `
-*_RHODVICK-TECH JUST LOGGED IN_*
-*_Made by Rhodvick Junior_*
+               let Ultraz_MD_TEXT = `
+*_Tennor-TECH JUST LOGGED IN_*
+*_Made by Giddy Tennor_*
 *_Session online_*
 ______________________________________
 ╔════◇
-║ *『 RHODVICK TECH LATEST AI 』*
+║ *『 TENNOR TECH LATEST AI 』*
 ║ _Deploy your bot now to enjoy the commands._
 ╰═════════════⊷
 ╔═════◇
 ║  『••• OWNER INFO•••』
-║❒ *Instagram:* _https://www.instagram.com/rhodvickjunior?igsh=YzljYTk1ODg3Zg==_
-║❒ *Owner:* _https://wa.me/254115790827_
-║❒ *Repo:* _https://github.com/Rhodvick/RHODVICK-AI_
-║❒ *WaGroup:* _https://chat.whatsapp.com/Dc7OCtGmAI2BNtkKbGSJyr_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D_
-║❒ *YouTube:* _https://www.youtube.com/@Rhodvick_
+║❒ *Instagram:* _https://https://www.instagram.com/giddy_owis_?igsh=YzZ0NDRoaXFxM2Zk_
+║❒ *Owner:* _https://wa.me/254703726139_
+║❒ *Repo:* _https://github.com/gidiowis/Ultraz_Al_
+║❒ *WaGroup:* _https://chat.whatsapp.com/CPH3QIk6MMMH7iHIZTO8OE_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VadqmtNA89MlYOW7Ep2n_
+║❒ *YouTube:* _https://youtube.com/@gideonowino-gc2ry?si=RnyrNnfzyt9s3Gqo_
 ╰═════════════⊷ 
 _____________________________________
 
 _Don't Forget To Give a Star To My Repo_`
- await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id,{text:FLASH_MD_TEXT},{quoted:session})
+ await Pair_Code_By_Giddy_Tennor.sendMessage(Pair_Code_By_Giddy_Tennor.user.id,{text:Ultraz_MD_TEXT},{quoted:session})
  
 
         await delay(100);
-        await Pair_Code_By_France_King.ws.close();
+        await Pair_Code_By_Giddy_Tennor.ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    FLASH_MD_PAIR_CODE();
+                    Ultraz_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
@@ -96,6 +96,6 @@ _Don't Forget To Give a Star To My Repo_`
          }
         }
     }
-    return await FLASH_MD_PAIR_CODE()
+    return await Ultraz_MD_PAIR_CODE()
 });
 module.exports = router
